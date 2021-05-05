26d9ff4ee9de109d1e6d8f8a2a47b66f228671ba
Osmar da Cunha Filho <Osmar da Cunha Filho>
2021-05-04 22:22:16 -0300

    GeneratedValue do id (auto-increment)

--- src/main/java/br/edu/usj/ads/pw/calculadorahistorico/Operacao.java
+++ src/main/java/br/edu/usj/ads/pw/calculadorahistorico/Operacao.java
@@ -1,6 +1,8 @@
 package br.edu.usj.ads.pw.calculadorahistorico;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -14,6 +16,7 @@ import lombok.Setter;
 public class Operacao {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
 
     String descricao;
