5da77a1bb3279bbd7c1cfc0cc7550cd13661b843
Osmar da Cunha Filho <Osmar da Cunha Filho>
2021-05-04 22:22:40 -0300

    findAll para tipo List (em vez de Iterable)

--- src/main/java/br/edu/usj/ads/pw/calculadorahistorico/OperacaoRepository.java
+++ src/main/java/br/edu/usj/ads/pw/calculadorahistorico/OperacaoRepository.java
@@ -1,7 +1,9 @@
 package br.edu.usj.ads.pw.calculadorahistorico;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface OperacaoRepository extends CrudRepository<Operacao, Long> {
-    
+    List<Operacao> findAll();
 }
