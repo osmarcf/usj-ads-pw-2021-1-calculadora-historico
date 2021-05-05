fd82a1b2a0c65669c71d74d654defb84cef79062
Osmar da Cunha Filho <Osmar da Cunha Filho>
2021-05-04 22:25:49 -0300

    todas as alteracoes do controller para usar o banco

--- src/main/java/br/edu/usj/ads/pw/calculadorahistorico/CalculadoraHistorico.java
+++ src/main/java/br/edu/usj/ads/pw/calculadorahistorico/CalculadoraHistorico.java
@@ -3,6 +3,7 @@ package br.edu.usj.ads.pw.calculadorahistorico;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class CalculadoraHistorico {
     
-    List<String> historico = new ArrayList<>();
+    // List<String> historico = new ArrayList<>();
+
+    @Autowired
+    OperacaoRepository historico;
 
     @PostMapping(value="calcular")
     public ModelAndView postCalcular(@RequestParam String operando1, @RequestParam String operando2, @RequestParam String operador) {
@@ -28,18 +32,26 @@ public class CalculadoraHistorico {
         }
 
         // colocar a operacao no formato
-        String operacao = operando1 + " " + operador + " " + operando2 + " = " + resultado;
+        String operacaoString = operando1 + " " + operador + " " + operando2 + " = " + resultado;
+
+        // criar operacao e preencher com a descricao que a representa
+        Operacao operacao = new Operacao();
+        operacao.setDescricao(operacaoString);
 
         // salvar operacao no historico
-        historico.add(operacao);
+        // historico.add(operacao); // ArrayList
+        historico.save(operacao);
 
         // instanciar template
         ModelAndView modelAndView = new ModelAndView("index");
 
+        // criar lista com as operacoes vindas do repository
+        List<Operacao> lista = historico.findAll();
+
         // aplicar resultado da operacao no template
         // aplicar o historico no template
         modelAndView.addObject("resultado", resultado);
-        modelAndView.addObject("historico", historico);
+        modelAndView.addObject("historico", lista);
 
         // retornar
         return modelAndView;
